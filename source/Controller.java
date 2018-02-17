51fd1b7822992de05fb62a0404e4ecf8aa655cd2
imullika1 <imullika1@gmail.com>
2018-02-17 18:34:33 +0300

    Skytale decode & encode

--- source/Controller.java
+++ source/Controller.java
@@ -3,26 +3,106 @@ import javafx.collections.ObservableList;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.util.Arrays;
 
 public class Controller {
     public TextArea textInput;
     public TextArea textOutput;
     public ChoiceBox<String> actionSelect;
-    public ChoiceBox algoritmSelect;
+    public ChoiceBox<String> algorithmSelect;
     public Button startButton;
+    public TextField keyInput;
 
     private ObservableList<String> actionItems = FXCollections.observableArrayList("Зашифровать", "Расшивровать");
+    private ObservableList<String> algorithmItems = FXCollections.observableArrayList("Скитала");
 
     public void initialize() {
         // Setup Action ChoiceBox
         actionSelect.setItems(actionItems);
         actionSelect.setValue(actionItems.get(0));
 
+        // Setup Algorithm ChoiceBox
+        algorithmSelect.setItems(algorithmItems);
+        algorithmSelect.setValue(algorithmItems.get(0));
+
         // Setup Start Button
         startButton.setOnMouseClicked(event -> onStartButtonClicked());
     }
 
     private void onStartButtonClicked() {
+        if (actionSelect.getSelectionModel().getSelectedIndex() == 0) {
+            if (algorithmSelect.getSelectionModel().getSelectedIndex() == 0) encodeUsingScytale();
+        } else {
+            if (algorithmSelect.getSelectionModel().getSelectedIndex() == 0) decodeUsingScytale();
+        }
+    }
 
+    private void encodeUsingScytale() {
+        // Calculate num of rows for char array
+        double calculatedRows = Math.ceil((double) textInput.getText().length() / Integer.parseInt(keyInput.getText()));
+        // Init double char array
+        char decodedArray[][] = new char[(int) calculatedRows][Integer.parseInt(keyInput.getText())];
+        // Counters for loop
+        int col = 0;
+        int row = 0;
+        // Make char array
+        for (int i = 0; i < textInput.getText().length(); i++) {
+            decodedArray[row][col] = textInput.getText().charAt(i);
+            if (col == Integer.parseInt(keyInput.getText()) - 1) {
+                col = 0;
+                row++;
+            }
+            else col++;
+        }
+        // Encode Build output string
+        StringBuilder encoded = new StringBuilder();
+        for (int cols = 0; cols < decodedArray[0].length; cols++) {
+            for (char[] DecodedArray : decodedArray) {
+                encoded.append(DecodedArray[cols]);
+            }
+        }
+
+        textOutput.setText(encoded.toString());
+    }
+
+    private void decodeUsingScytale() {
+        // Init string to decode
+        String encodedString = textInput.getText();
+        // Init string builder for format new sting
+        StringBuilder decodedStringBuilder = new StringBuilder();
+        // Calculate num of rows for char array
+        double calculatedRows = Math.ceil((double) textInput.getText().length() / Integer.parseInt(keyInput.getText()));
+        // Init double char array
+        char decodedArray[][] = new char[(int) calculatedRows][Integer.parseInt(keyInput.getText())];
+        // Counters for loop
+        int col = 0;
+        int row = 0;
+        // Decode loop
+        for (int times = 0; times < Integer.parseInt(keyInput.getText()); times++) {
+            col = 0;
+            row = 0;
+            for (int i = 0; i < encodedString.length(); i++) {
+                decodedArray[row][col] = encodedString.charAt(i);
+                if (col == Integer.parseInt(keyInput.getText()) - 1) {
+                    col = 0;
+                    row++;
+                }
+                else col++;
+            }
+
+            for (int cols = 0; cols < decodedArray[0].length; cols++) {
+                for (char[] DecodedArray : decodedArray) {
+                    decodedStringBuilder.append(DecodedArray[cols]);
+                }
+            }
+
+            encodedString = decodedStringBuilder.toString();
+            decodedStringBuilder = new StringBuilder();
+            System.out.println(encodedString);
+        }
+
+        textOutput.setText(encodedString);
     }
 }
