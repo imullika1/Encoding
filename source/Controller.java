e62e03552490c824ef70c2d609575f5d51a59219
imullika1 <imullika1@gmail.com>
2018-02-20 22:35:46 +0300

    Fully worked Scytale Encoding & Decoding with error handlers

--- source/Controller.java
+++ source/Controller.java
@@ -1,58 +1,107 @@
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Controller {
-    public Button startButton;
-    public TextField keyInput;
-    public TextArea textInput;
-    public TextArea textOutput;
-    public TextArea errorOutput;
-    public ChoiceBox<String> actionSelect;
-    public ChoiceBox<String> algorithmSelect;
-    public TextField keyInput2;
-
-    // Setup choice boxes
-    private ObservableList<String> actionItems = FXCollections.observableArrayList("Зашифровать", "Расшивровать");
-    private ObservableList<String> algorithmItems = FXCollections.observableArrayList("Скитала", "Вертикальная перестановка", "Двойная перестановка");
+    // UI Elements
+    public Button start;
+    public Label error;
+    public Label counter;
+    public TextArea input;
+    public TextArea output;
+    public TextField key_1;
+    public TextField key_2;
+    public ChoiceBox<String> action;
+    public ChoiceBox<String> algorithm;
 
     // Setup on startup
     public void initialize() {
+
         // Setup Action ChoiceBox
-        actionSelect.setItems(actionItems);
-        actionSelect.setValue(actionItems.get(0));
+        action.setItems(FXCollections.observableArrayList(
+                "Зашифровать", "Расшивровать"
+        ));
+        action.getSelectionModel().select(0);
 
         // Setup Algorithm ChoiceBox
-        algorithmSelect.setItems(algorithmItems);
-        algorithmSelect.setValue(algorithmItems.get(0));
+        algorithm.setItems(FXCollections.observableArrayList(
+                "Скитала", "Вертикальная перестановка", "Двойная перестановка"
+        ));
+        algorithm.getSelectionModel().select(0);
+
+        // Input text length counter
+        input
+                .textProperty()
+                .addListener((observable, oldValue, newValue) -> {
+                    counter.setText("Количество символов: " + input.getLength());
+                });
+
+        // Algorithm select event
+        algorithm
+                .getSelectionModel()
+                .selectedIndexProperty()
+                .addListener((observable, oldValue, newValue) -> {
+                    if (newValue.equals(2)) key_2.setVisible(true);
+                    else key_2.setVisible(false);
+                });
+
+        // Input key rules
+        key_1.textProperty().addListener((observable, oldValue, newValue) -> checkKeyRules(oldValue, newValue));
 
         // Setup Start Button
-        startButton.setOnMouseClicked(event -> onStartButtonClicked());
+        start.setOnMouseClicked(event -> checkRules());
     }
 
-    // Start button click event
-    private void onStartButtonClicked() {
-        // Check what type of action chosen: encoding/decoding
-        if (actionSelect.getSelectionModel().getSelectedIndex() == 0) {
-            // If encoding - check the method of encoding
-            if (algorithmSelect.getSelectionModel().getSelectedIndex() == 0) encodeUsingScytale();
-            else if (algorithmSelect.getSelectionModel().getSelectedIndex() == 1) encodeUsingVerticalReplacement();
-            else if (algorithmSelect.getSelectionModel().getSelectedIndex() == 2) encodeUsingDoubleReplacement();
-        } else {
-            // If decoding - check the method of decoding
-            if (algorithmSelect.getSelectionModel().getSelectedIndex() == 0) decodeUsingScytale();
-            else if (algorithmSelect.getSelectionModel().getSelectedIndex() == 1) decodeUsingVerticalReplacement();
-            else if (algorithmSelect.getSelectionModel().getSelectedIndex() == 2) decodeUsingDoubleReplacement();
-        }
+    // Check key input rules
+    private void checkKeyRules(String oldValue, String newValue) {
+        if (newValue.length() != 0) {
+
+            // Null error message
+            error.setText("");
+
+            // Set start button visible
+            start.setVisible(true);
+
+            // Set algorithm variable
+            Integer algorithm = this.algorithm.getSelectionModel().getSelectedIndex();
+
+            // Check scytale key input rules
+            if (algorithm == 0) {
+                if (!newValue.chars().allMatch(element -> newValue.matches("^[0-9]+$"))) {
+                    key_1.setText(oldValue);
+                    error.setText(
+                        "Для метода шифрования скитала" +
+                        "требуется указать ключ одним" +
+                        "десятичным числом без пробелов и символов."
+                    );
+                } else key_1.setText(newValue);
+            }
+        } else start.setVisible(false);
+
+    }
+
+    private void checkRules() {
+        String key_1 = this.key_1.getText();
+        String input = this.input.getText();
+        Integer action = this.action.getSelectionModel().getSelectedIndex();
+        Integer algorithm = this.algorithm.getSelectionModel().getSelectedIndex();
+
+        if (input.length() != 0) {
+            if (algorithm == 0) {
+                if (Integer.parseInt(key_1) != 0 && Integer.parseInt(key_1) != 1)
+                    if (input.length() > Integer.parseInt(key_1) && input.length() != Integer.parseInt(key_1)) {
+                        Scytale scytale = new Scytale(input, Integer.parseInt(key_1));
+                        if (action == 0) output.setText(scytale.encode());
+                        else output.setText(scytale.decode());
+                    } else error.setText("Использование длины ключа больше или равной длине текста бесполезно.");
+                else error.setText("Ключ не должен быть равен 0 или 1.");
+            }
+        } else error.setText("Введите текст для шифровки/расшифровки");
     }
 
     // Double Encoding & Decoding
-    private void encodeUsingDoubleReplacement () {
+    private void encodeUsingDoubleReplacement() {
 
         // Init horizontal key array
         List<Integer> horizontalKeyArray = new ArrayList<>();
@@ -60,14 +109,16 @@ public class Controller {
         List<Integer> verticalKeyArray = new ArrayList<>();
 
         // Reformat horizontal key input to string array
-        String[] horizontalKeyStringArray = keyInput.getText().replaceAll("\\s", "").split(",");
+        String[] horizontalKeyStringArray = key_1.getText().replaceAll("\\s", "").split(",");
         // Reformat vertical key input to string array
-        String[] verticalKeyStringArray = keyInput2.getText().replaceAll("\\s", "").split(",");
+        String[] verticalKeyStringArray = key_2.getText().replaceAll("\\s", "").split(",");
 
         // Filling up horizontalKeyArray
-        for (String horizontalStringyInteger: horizontalKeyStringArray) horizontalKeyArray.add(Integer.parseInt(horizontalStringyInteger));
+        for (String horizontalStringyInteger : horizontalKeyStringArray)
+            horizontalKeyArray.add(Integer.parseInt(horizontalStringyInteger));
         // Filling up verticalKeyArray
-        for (String verticalStringyInteger: verticalKeyStringArray) verticalKeyArray.add(Integer.parseInt(verticalStringyInteger));
+        for (String verticalStringyInteger : verticalKeyStringArray)
+            verticalKeyArray.add(Integer.parseInt(verticalStringyInteger));
 
         // Setup horizontal key length
         Integer horizontalKeyLength = horizontalKeyArray.size();
@@ -75,64 +126,62 @@ public class Controller {
         Integer verticalKeyLength = verticalKeyArray.size();
 
         // Compare for input text and key length
-        if (horizontalKeyLength > textInput.getLength()) errorOutput.setText("Ключ не может быть длиннее текста.");
+
         // Setup decoded double dimensional array
-        else {
-            // Init counter
-            Integer counter = 0;
 
-            // Init string builder for format encoded output string
-            StringBuilder encodedString = new StringBuilder();
+        // Init counter
+        Integer counter = 0;
 
-            // Init single dimensional array
-            List<Character> colCharacterSingleDimensionalArray = new ArrayList<>();
+        // Init string builder for format encoded output string
+        StringBuilder encodedString = new StringBuilder();
 
-            // Init double dimensional array
-            List<List<Character>> rowCharacterDoubleDimensionalArray = new ArrayList<>();
+        // Init single dimensional array
+        List<Character> colCharacterSingleDimensionalArray = new ArrayList<>();
 
-            // Format single & double dimensional arrays
-            for (int i = 0; i < textInput.getLength(); i++) {
-                colCharacterSingleDimensionalArray.add(textInput.getText().charAt(i));
-                if (counter != horizontalKeyLength - 1) counter++;
-                else {
-                    rowCharacterDoubleDimensionalArray.add(new ArrayList<>(colCharacterSingleDimensionalArray));
-                    colCharacterSingleDimensionalArray.clear();
-                    counter = 0;
-                }
+        // Init double dimensional array
+        List<List<Character>> rowCharacterDoubleDimensionalArray = new ArrayList<>();
+
+        // Format single & double dimensional arrays
+        for (int i = 0; i < input.getLength(); i++) {
+            colCharacterSingleDimensionalArray.add(input.getText().charAt(i));
+            if (counter != horizontalKeyLength - 1) counter++;
+            else {
+                rowCharacterDoubleDimensionalArray.add(new ArrayList<>(colCharacterSingleDimensionalArray));
+                colCharacterSingleDimensionalArray.clear();
+                counter = 0;
             }
-            // Prepare counter for vertical encoding
-            counter = 1;
-
-            // Init vertical encoded single dimensional array
-            List<Object> colVerticalEncodedSingleDimensionalArray = new ArrayList<>();
-            // Init vertical encoded double dimensional array
-            List<List<Object>> rowVerticalEncodedDoubleDimensionalArray = new ArrayList<>();
-
-            // Format vertical encoding
-            for (int i = 0; i < horizontalKeyLength; i++) {
-                for (List list: rowCharacterDoubleDimensionalArray) {
-                    colVerticalEncodedSingleDimensionalArray.add(list.get(horizontalKeyArray.indexOf(counter)));
-                }
-                rowVerticalEncodedDoubleDimensionalArray.add(new ArrayList<>(colVerticalEncodedSingleDimensionalArray));
-                colVerticalEncodedSingleDimensionalArray.clear();
-                counter++;
-            }
-
-            // Prepare counter for horizontal encoding
-            counter = 1;
-
-            // Format horizontal encoding
-            for (int i = 0; i < verticalKeyLength; i++) {
-                for (List list: rowVerticalEncodedDoubleDimensionalArray) {
-                    encodedString.append(list.get(verticalKeyArray.indexOf(counter)));
-                }
-                counter++;
-            }
-
-            textOutput.setText(encodedString.toString());
         }
+        // Prepare counter for vertical encoding
+        counter = 1;
+
+        // Init vertical encoded single dimensional array
+        List<Object> colVerticalEncodedSingleDimensionalArray = new ArrayList<>();
+        // Init vertical encoded double dimensional array
+        List<List<Object>> rowVerticalEncodedDoubleDimensionalArray = new ArrayList<>();
+
+        // Format vertical encoding
+        for (int i = 0; i < horizontalKeyLength; i++) {
+            for (List list : rowCharacterDoubleDimensionalArray) {
+                colVerticalEncodedSingleDimensionalArray.add(list.get(horizontalKeyArray.indexOf(counter)));
+            }
+            rowVerticalEncodedDoubleDimensionalArray.add(new ArrayList<>(colVerticalEncodedSingleDimensionalArray));
+            colVerticalEncodedSingleDimensionalArray.clear();
+            counter++;
+        }
+
+        // Prepare counter for horizontal encoding
+        counter = 1;
+
+        // Format horizontal encoding
+        for (int i = 0; i < verticalKeyLength; i++) {
+            for (List list : rowVerticalEncodedDoubleDimensionalArray) {
+                encodedString.append(list.get(verticalKeyArray.indexOf(counter)));
+            }
+            counter++;
+        }
+
+        output.setText(encodedString.toString());
     }
-    private void decodeUsingDoubleReplacement () { }
 
     // Vertical Encoding & Decoding
     private void encodeUsingVerticalReplacement() {
@@ -140,7 +189,7 @@ public class Controller {
         List<Integer> integerKeyArray = new ArrayList<>();
 
         // Reformat key input to string array
-        String[] reformatedStringArray = keyInput.getText().replaceAll("\\s", "").split(",");
+        String[] reformatedStringArray = key_1.getText().replaceAll("\\s", "").split(",");
 
         // Reformat string array to integer array
         for (String stringInteger : reformatedStringArray) integerKeyArray.add(Integer.parseInt(stringInteger));
@@ -149,90 +198,43 @@ public class Controller {
         Integer keyLength = integerKeyArray.size();
 
         // Check for input text and key length
-        if (keyLength > textInput.getText().length()) errorOutput.setText("Ключ не может быть длиннее текста.");
+
         // Format decoded double dimensional array
-        else {
-            // Init counter
-            Integer counter = 0;
 
-            // Init string builder for format encoded output string
-            StringBuilder encodedString = new StringBuilder();
+        // Init counter
+        Integer counter = 0;
 
-            // Setup single dimensional array
-            List<Character> colCharacterSingleDimensionalArray = new ArrayList<>();
+        // Init string builder for format encoded output string
+        StringBuilder encodedString = new StringBuilder();
 
-            // Setup double dimensional array
-            List<List<Character>> rowCharacterDoubleDimensionalArray = new ArrayList<>();
+        // Setup single dimensional array
+        List<Character> colCharacterSingleDimensionalArray = new ArrayList<>();
 
-            // Format single & double dimensional arrays
-            for (int i = 0; i < textInput.getLength(); i++) {
-                colCharacterSingleDimensionalArray.add(textInput.getText().charAt(i));
-                if (counter == keyLength - 1) {
-                    rowCharacterDoubleDimensionalArray.add(new ArrayList<>(colCharacterSingleDimensionalArray));
-                    colCharacterSingleDimensionalArray.clear();
-                    counter = 0;
-                } else counter++;
-            }
+        // Setup double dimensional array
+        List<List<Character>> rowCharacterDoubleDimensionalArray = new ArrayList<>();
 
-            // Prepare counter for new loop
-            counter = 1;
-
-            // Format encoded string for output
-            for (int index = 0; index < keyLength; index++) {
-                for (List list : rowCharacterDoubleDimensionalArray) {
-                    encodedString.append(list.get(integerKeyArray.indexOf(counter)));
-                }
-                counter++;
-            }
-
-            // Output encoded string
-            textOutput.setText(encodedString.toString());
-        }
-    }
-    private void decodeUsingVerticalReplacement() { }
-
-    // Scytale Encoding & Decoding
-    private void encodeUsingScytale() {
-        // Setup key
-        Integer key = Integer.parseInt(keyInput.getText());
-
-        // Check key & input string length
-        if (textInput.getLength() < key) errorOutput.setText("Ключ не может быть длинее текста");
-        else {
-            // Init counter
-            Integer counter = 1;
-
-            // Init string builder for format encoded output string
-            StringBuilder encodedString = new StringBuilder();
-
-            // Setup single dimensional array of cols
-            List<Character> colCharacterSingleDimensionalArray = new ArrayList<>();
-
-            // Setup double dimensional array of rows
-            List<List<Character>> rowCharacterDoubleDimensionalArray = new ArrayList<>();
-
-            // Format decoded double dimensional array
-            for (int i = 0; i < textInput.getLength(); i++) {
-                colCharacterSingleDimensionalArray.add(textInput.getText().charAt(i));
-                if (!counter.equals(key)) counter++;
-                else {
-                    rowCharacterDoubleDimensionalArray.add(new ArrayList<>(colCharacterSingleDimensionalArray));
-                    colCharacterSingleDimensionalArray.clear();
-                    counter = 1;
-                }
-            }
-
-            // Format encoded string for output
-            for (int i = 0; i < rowCharacterDoubleDimensionalArray.get(0).size(); i++) {
-                for (List row: rowCharacterDoubleDimensionalArray) {
-                    encodedString.append(row.get(i));
-                }
-            }
-
-            // Output encoded string
-            textOutput.setText(encodedString.toString());
+        // Format single & double dimensional arrays
+        for (int i = 0; i < input.getLength(); i++) {
+            colCharacterSingleDimensionalArray.add(input.getText().charAt(i));
+            if (counter == keyLength - 1) {
+                rowCharacterDoubleDimensionalArray.add(new ArrayList<>(colCharacterSingleDimensionalArray));
+                colCharacterSingleDimensionalArray.clear();
+                counter = 0;
+            } else counter++;
         }
 
+        // Prepare counter for new loop
+        counter = 1;
+
+        // Format encoded string for output
+        for (int index = 0; index < keyLength; index++) {
+            for (List list : rowCharacterDoubleDimensionalArray) {
+                encodedString.append(list.get(integerKeyArray.indexOf(counter)));
+            }
+            counter++;
+        }
+
+        // Output encoded string
+        output.setText(encodedString.toString());
     }
-    private void decodeUsingScytale() { }
 }
---
+++ source/Scytale.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Scytale {
+    // Variables
+    private Integer key;
+    private String input;
+    private StringBuilder output = new StringBuilder();
+    private List<List<Character>> rows = new ArrayList<>();
+
+    // Constructor
+    Scytale(String input, Integer key) {
+        this.key = key;
+        this.input = input;
+        this.init();
+    }
+
+    // Setup rows of input text
+    private void init () {
+        for (String string : input.split("(?<=\\G.{" + key + "})")) {
+            List<Character> charArray = new ArrayList<>();
+            for (int i = 0; i < string.length(); i++) charArray.add(string.charAt(i));
+            rows.add(new ArrayList<>(charArray));
+        }
+    }
+
+    // Encoding
+    String encode () {
+        for (int i = 0; i < rows.get(0).size(); i++) {
+            for (List row: rows) {
+                try { output.append(row.get(i)); }
+                catch (IndexOutOfBoundsException ignored) { output.append("*"); }
+            }
+        }
+        return  String.valueOf(output);
+    }
+
+    // Decoding
+    String decode () {
+        String[] secondArrayOfStrings = input.split("(?<=\\G.{" + rows.size() + "})");
+        for (int i = 0; i < secondArrayOfStrings[0].length(); i++) {
+            for (String string: secondArrayOfStrings) {
+                output.append(string.charAt(i));
+            }
+        }
+        return  String.valueOf(output.toString().replace("*", ""));
+    }
+
+}
